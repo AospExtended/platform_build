228f91654cfaa7b8d31f494884fd1db081ba46c8
Steve Kondik <shade@chemlab.org>
2010-04-21 11:39:48 -0400

    Allow override of device asserts, including multi-device support.
    Set in board file with TARGET_OTA_ASSERT_DEVICE.
    (cherry-picked from commit 0f452f21fc9323b9d1fe746161761cf40aaa5030)
    
    Change-Id: I3d06bdc0e3e26bde0c0e646accd050364f9713b9
    
    ota_from_target_files: Remove device dependent arguments
    
    These device-specific arguments are defined at build time and are
    necessary to generate the zip correctly. Don't use command line
    arguments to specify them, but write all the needed information
    in misc_info.txt when the target-files zip is generated.
    ota_from_target_files will then read misc_info.txt and set
    everything automatically.
    
    Change-Id: Ibdbca575b76eb07b53fccfcea52a351c7e333f91
    Signed-off-by: André Pinela <sheffzor@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -93,7 +93,7 @@ class Options(object):
     self.cache_size = None
     self.stash_threshold = 0.8
     self.logfile = None
-
+    self.override_device = 'auto'
 
 OPTIONS = Options()
 
@@ -390,7 +390,10 @@ class BuildInfo(object):
           "system_other"] = self._partition_fingerprints["system"]
 
     # These two should be computed only after setting self._oem_props.
-    self._device = self.GetOemProperty("ro.product.device")
+    if OPTIONS.override_device == "auto":
+      self._device = self.GetOemProperty("ro.product.device")
+    else:
+      self._device = OPTIONS.override_device
     self._fingerprint = self.CalculateFingerprint()
     check_fingerprint(self._fingerprint)
 
@@ -1999,6 +2002,9 @@ Global options
 
   --logfile <file>
       Put verbose logs to specified file (regardless of --verbose option.)
+
+  --override_device <device>
+      Override device-specific asserts. Can be a comma-separated list.
 """
 
 def Usage(docstring):
@@ -2025,7 +2031,7 @@ def ParseOptions(argv,
          "private_key_suffix=", "boot_signer_path=", "boot_signer_args=",
          "verity_signer_path=", "verity_signer_args=", "device_specific=",
          "extra=", "logfile=", "aftl_server=", "aftl_key_path=",
-         "aftl_manufacturer_key_path=", "aftl_signer_helper="] +
+         "aftl_manufacturer_key_path=", "aftl_signer_helper=", "override_device"] +
         list(extra_long_opts))
   except getopt.GetoptError as err:
     Usage(docstring)
@@ -2079,6 +2085,8 @@ def ParseOptions(argv,
       OPTIONS.extras[key] = value
     elif o in ("--logfile",):
       OPTIONS.logfile = a
+    elif o in ("--override_device"):
+      OPTIONS.override_device = a
     else:
       if extra_option_handler is None or not extra_option_handler(o, a):
         assert False, "unknown option \"%s\"" % (o,)
