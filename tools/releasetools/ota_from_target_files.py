019e46e3e2d7102333ebcdd2a9376c35063aa02a
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-07 17:28:49 +0100

    Store the base64 release key in the OTA zips
    This can be directly grepped in pre-existing package.xml tables
    to make sure we're not trying to update to a differently signed
    build
    
    Change-Id: I7528a8e7c484ea9209cd665b9263328ae834586a

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -637,6 +637,8 @@ endif;
   metadata["ota-required-cache"] = str(script.required_cache)
   WriteMetadata(metadata, output_zip)
 
+  common.ZipWriteStr(output_zip, "META-INF/org/aospextended/releasekey",
+                     ""+input_zip.read("META/releasekey.txt"))
 
 def WritePolicyConfig(file_name, output_zip):
   common.ZipWrite(output_zip, file_name, os.path.basename(file_name))
