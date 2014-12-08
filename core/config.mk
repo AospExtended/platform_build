ed6e09aefda6936a8bdd232edd50d0be1c55671c
Steve Kondik <steve@cyngn.com>
2014-12-07 16:42:28 -0800

    build: Parse TARGET_COPY_FILES_OVERRIDES late in the setup
    Change-Id: Ie3e4a168ff224e3b65e4627c0757e8813a02167f

--- core/config.mk
+++ core/config.mk
@@ -877,4 +877,15 @@ RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
+# We might want to skip items listed in PRODUCT_COPY_FILES based on
+# various target flags. This is useful for replacing a binary module with one
+# built from source. This should be a list of destination files under $OUT
+#
+TARGET_COPY_FILES_OVERRIDES := \
+    $(addprefix %:, $(strip $(TARGET_COPY_FILES_OVERRIDES)))
+
+ifneq ($(TARGET_COPY_FILES_OVERRIDES),)
+    PRODUCT_COPY_FILES := $(filter-out $(TARGET_COPY_FILES_OVERRIDES), $(PRODUCT_COPY_FILES))
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
