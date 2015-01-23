7e0fd0b7fd241d9b6bcac7c656ee9f18f5326392
Chet Kener <Cl3Kener@gmail.com>
2015-01-23 10:32:21 -0500

    Do Not Odex Eng Builds
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -438,6 +438,7 @@ endif # !enable_target_debugging
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 tags_to_install := debug eng
+WITH_DEXPREOPT := false
 ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))),)
   # Don't require the setup wizard on eng builds
   ADDITIONAL_BUILD_PROPERTIES := $(filter-out ro.setupwizard.mode=%,\
