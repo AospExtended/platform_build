4caca19cddd688b92d7374be64e89effa76a5246
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    Build Google audio files from android12 [1/2]
    Change-Id: Ib20341d418c9f827ab86e60d52724d96b8d7e31a
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -19,12 +19,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, vendor/aosp/config/google_audio.mk)
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
-    ro.config.notification_sound?=pixiedust.ogg \
+    ro.config.ringtone?=The_big_adventure.ogg \
+    ro.config.notification_sound?=Popcorn.ogg \
     ro.com.android.dataroaming?=true \
 
 # More AOSP packages
