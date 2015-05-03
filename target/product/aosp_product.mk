378de60c980571ecdb4b2ddfc2a8e5f20820afc5
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    Build Google audio files
    - which are in vendor/aosp
    - hence stop copying aosp audio files
    
    Change-Id: Ib20341d418c9f827ab86e60d52724d96b8d7e31a
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -18,14 +18,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(AOSP_BUILD),)
 # Default AOSP sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg \
-    ro.com.android.dataroaming=true \
+    ro.config.notification_sound=pixiedust.ogg
+endif
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.android.dataroaming=true
 
 # More AOSP packages
 PRODUCT_PACKAGES += \
