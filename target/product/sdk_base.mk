767bcb884fc9a67214c4826efe331ca11799d88d
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    Use Google Nexus Audio Files [1/2]
    Change-Id: I11528ac9c838ebef14c889f13f34dda458b2f7a4
    
    Conflicts:
    	target/product/sdk_base.mk

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -79,7 +79,7 @@ PRODUCT_COPY_FILES := \
 
 include $(SRC_TARGET_DIR)/product/emulator.mk
 
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/dancing-script/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/carrois-gothic-sc/fonts.mk)
