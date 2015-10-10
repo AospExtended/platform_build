4821dd27b41b1df00b0f88165859916aa9907fc7
Corey Edwards <ensabahnur16@gmail.com>
2015-10-10 14:38:39 -0500

    [SQUASHED] Roomservice from SlimRoms
    Change-Id: Ie605faba5837a0db4339d7fc1340724105f37fd4
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -612,6 +612,17 @@ function lunch()
     build_build_var_cache
     if [ $? -ne 0 ]
     then
+         # if we can't find a product, try to grab it off the AOSP JDC github
+        T=$(gettop)
+        pushd $T > /dev/null
+        build/tools/roomservice.py $product
+        popd > /dev/null
+        check_product $product
+    else
+        build/tools/roomservice.py $product true
+    fi
+    if [ $? -ne 0 ]
+    then
         return 1
     fi
 
