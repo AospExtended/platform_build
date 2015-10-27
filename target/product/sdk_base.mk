764903a79b04023aeeb4e274fe3491293755771c
Legrand Benjamin <android@legrand.ws>
2015-10-27 23:12:06 +0100

    Do not use the default APN. Device should provide it's own apns-conf.xml
    Change-Id: Ib245e72d5ffe1b874e6db031d0afe2d7c97050f1
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -60,7 +60,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGE_OVERLAYS := development/sdk_overlay
 
 PRODUCT_COPY_FILES := \
-	device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
 	device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
 	device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
