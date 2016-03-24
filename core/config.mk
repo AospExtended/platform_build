7764bf4bd91781cb57e5bf29843c926fba4aceea
srisurya95 <srisurya95@gmail.com>
2016-03-24 21:29:45 +0530

    Modifiy for aosp

--- core/config.mk
+++ core/config.mk
@@ -915,10 +915,9 @@ ifneq ($(TARGET_COPY_FILES_OVERRIDES),)
     PRODUCT_COPY_FILES := $(filter-out $(TARGET_COPY_FILES_OVERRIDES), $(PRODUCT_COPY_FILES))
 endif
 
-ifneq ($(SLIM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/slim/sepolicy/sepolicy.mk)
+$(eval include vendor/aosp/sepolicy/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
@@ -926,6 +925,5 @@ $(eval include vendor/slim/sepolicy/sepolicy.mk)
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
-endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
