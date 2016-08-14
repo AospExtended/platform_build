131589f0fb024f06809c9bac9616722e66e25d0f
Akhil Narang <akhilnarang.1999@gmail.com>
2016-08-14 16:51:31 +0530

    repopick: fetch only from gerrit, not from github
    Change-Id: Idd7ec7097537d8528cf82af036f38da3411ee40d
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- tools/repopick.py
+++ tools/repopick.py
@@ -385,25 +385,14 @@ for changeps in args.change_number:
         print('--> Author:        %s <%s> %s' % (author_name, author_email, author_date))
         print('--> Committer:     %s <%s> %s' % (committer_name, committer_email, committer_date))
 
-    # Try fetching from GitHub first
+    # Fetch the change from Gerrit
     if args.verbose:
-       print('Trying to fetch the change from GitHub')
+      print('Fetching the change from Gerrit')
     if args.pull:
-      cmd = 'cd %s && git pull --no-edit github %s' % (project_path, fetch_ref)
+      cmd = 'cd %s && git pull --no-edit %s %s' % (project_path, fetch_url, fetch_ref)
     else:
-      cmd = 'cd %s && git fetch github %s' % (project_path, fetch_ref)
-    execute_cmd(cmd, True)
-    # Check if it worked
-    FETCH_HEAD = '%s/.git/FETCH_HEAD' % project_path
-    if os.stat(FETCH_HEAD).st_size == 0:
-        # That didn't work, fetch from Gerrit instead
-        if args.verbose:
-          print('Fetching from GitHub didn\'t work, trying to fetch the change from Gerrit')
-        if args.pull:
-          cmd = 'cd %s && git pull --no-edit %s %s' % (project_path, fetch_url, fetch_ref)
-        else:
-          cmd = 'cd %s && git fetch %s %s' % (project_path, fetch_url, fetch_ref)
-        execute_cmd(cmd)
+      cmd = 'cd %s && git fetch %s %s' % (project_path, fetch_url, fetch_ref)
+    execute_cmd(cmd)
     # Perform the cherry-pick
     cmd = 'cd %s && git cherry-pick FETCH_HEAD' % (project_path)
     if not args.pull:
