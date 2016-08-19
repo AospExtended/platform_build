9579bb9a5f07903032fb70152555f7d51512478a
B--B <dark.schneider.ds@gmail.com>
2016-08-19 16:06:17 +0200

    Installation script: advise the user that BackupTools is running

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -673,7 +673,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.Mount("/system")
+    script.Print("BackupTools: starting backup script")
     script.RunBackup("backup")
+    script.Print("BackupTools: DONE! Now real installation will begin")
     script.Unmount("/system")
 
   system_progress = 0.75
@@ -770,7 +772,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.ShowProgress(0.02, 10)
     if block_based:
       script.Mount("/system")
+    script.Print("BackupTools: Restoring backup")
     script.RunBackup("restore")
+    script.Print("BackupTools: DONE!")
     if block_based:
       script.Unmount("/system")
 
