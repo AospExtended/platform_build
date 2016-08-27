d53466bbeb1b3305c03842547af8b2bc88c4d3b4
Chet Kener <Cl3Kener@gmail.com>
2016-08-27 10:05:36 -0400

    Do not build test stuff for eng builds
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -1088,14 +1088,16 @@ target-java-tests : java-target-tests
 target-native-tests : native-target-tests
 tests : host-tests target-tests
 
-# To catch more build breakage, check build tests modules in eng and userdebug builds.
+# To catch more build breakage, check build tests modules in userdebug builds.
 ifneq ($(ANDROID_NO_TEST_CHECK),true)
 ifneq ($(TARGET_BUILD_PDK),true)
-ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
+ifneq ($(filter userdebug,$(TARGET_BUILD_VARIANT)),)
 droidcore : target-tests host-tests
 endif
 endif
 endif
+
+.PHONY: lintall
 
 ifneq (,$(filter samplecode, $(MAKECMDGOALS)))
 .PHONY: samplecode
