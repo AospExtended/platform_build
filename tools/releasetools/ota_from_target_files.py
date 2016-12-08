5217be8ab7bda46048ba3d3c708d4cc6dd356775
starkdroid <gamerprince.exp@gmail.com>
2016-12-08 16:50:00 +0530

    Revert "(2/2) SuperSU: move to install directory"
    This reverts commit d88f5d08d5a54fab941d93dac4bc488943ee1d67.

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -780,13 +780,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Install SuperSU. 
   
   script.Print("Extracting SuperSU...");
-  script.AppendExtra('package_extract_dir("install/supersu", "/tmp/supersu");')
+  script.AppendExtra('package_extract_dir("system/supersu", "/tmp/supersu");')
   script.AppendExtra('run_program("/sbin/busybox", "unzip", "/tmp/supersu/supersu.zip", "META-INF/com/google/android/update-binary", "-d", "/tmp/supersu");')
   script.Print("Installing SuperSU...");
   script.AppendExtra('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "null", "1", "/tmp/supersu/supersu.zip");')
 
   script.Print("Cleaning up...");
+  script.Mount("/system")
   script.AppendExtra('delete_recursive("/tmp/supersu");')
+  script.AppendExtra('delete_recursive("/system/supersu");')
+  script.Unmount("/system")
 
   script.Print("Installation complete!");
 
