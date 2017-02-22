22bc4bbfbc3e1e72ab450702d87914ebdf54303c
Abhisek Devkota <ciwrl@lineageos.org>
2017-02-22 13:36:48 -0800

    Add dumpvar for WITH_GMS
    This got dropped after caf-rebase and was missed in
      010ce6f3a21c73d1eb86c4558974d83e5edf5d5a
    
    Change-Id: I1a0d18e3c3c7b27a3e0d2590caec498e77bfeca6

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -29,6 +29,11 @@ print_build_config_vars += \
   RECOVERY_VARIANT
 endif
 
+ifeq ($(WITH_GMS),true)
+print_build_config_vars += \
+  WITH_GMS
+endif
+
 ifeq ($(TARGET_BUILD_PDK),true)
 print_build_config_vars += \
   TARGET_BUILD_PDK \
