ResurrectionRemix/android_build

Add hardware & software information to flash script
Based on this commit : https://github.com/AICP/build/commit/51eb7dd190cb26f15287ba84b10a6a9b2fa92591

tools/releasetools/ota_from_target_files.py
@@ -677,20 +677,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
-  if OPTIONS.wipe_user_data:
-    system_progress -= 0.1
-  if HasVendorPartition(input_zip):
-    system_progress -= 0.1
-
-  if not OPTIONS.wipe_user_data:
-    script.AppendExtra("if is_mounted(\"/data\") then")
-    script.ValidateSignatures("data")
-    script.AppendExtra("else")
-    script.Mount("/data")
-    script.ValidateSignatures("data")
-    script.Unmount("/data")
-    script.AppendExtra("endif;")
-
   script.Print(" ")
   script.Print(" ")
   script.Print(" ")
@@ -710,8 +696,62 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print(" R::::::R     R:::::R  R::::::R     R:::::R")
   script.Print(" R::::::R     R:::::R  R::::::R     R:::::R")
   script.Print(" RRRRRRRR     RRRRRRR  RRRRRRRR     RRRRRRR")
-  script.Print("  RR NOUGAT ")
   script.Print(" ")
+  script.Print(" ")
+  
+  if GetBuildProp("ro.rr.version", OPTIONS.info_dict) is not None:
+    buildid = GetBuildProp("ro.rr.version", OPTIONS.info_dict)
+    buildidn = GetBuildProp("ro.build.id", OPTIONS.info_dict)
+    buildday = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+    securep = GetBuildProp("ro.build.version.security_patch", OPTIONS.info_dict)
+    buildhst = GetBuildProp("ro.build.host", OPTIONS.info_dict)
+    density = GetBuildProp("ro.sf.lcd_density", OPTIONS.info_dict)
+    device = GetBuildProp("ro.rr.device", OPTIONS.info_dict)
+    androidver = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
+    manifacturer = GetBuildProp("ro.product.manufacturer", OPTIONS.info_dict)
+    maintainer = GetBuildProp("ro.build.user", OPTIONS.info_dict)
+    sdkver = GetBuildProp("ro.build.version.sdk", OPTIONS.info_dict)
+    script.Print(" **************** Software *****************");
+    script.Print(" OS ver: %s"%(buildid));
+    script.Print("");
+    script.Print(" Android ver: %s"%(androidver));
+    script.Print("");
+    script.Print(" Security patch: %s"%(securep));
+    script.Print("");
+    script.Print(" SDK ver: %s"%(sdkver));
+    script.Print("");
+    script.Print(" Root status: Enabled");
+    script.Print("");
+    script.Print(" Build ID: %s"%(buildidn));
+    script.Print("");
+    script.Print(" Build date: %s"%(buildday));
+    script.Print("");
+    script.Print(" Build host: %s"%(buildhst));
+    script.Print("");
+    script.Print(" Maintainer: %s"%(maintainer));
+    script.Print(" **************** Hardware *****************");
+    script.Print(" Device codename: %s"%(device));
+    script.Print("");
+    script.Print(" Manifacturer: %s"%(manifacturer));
+    script.Print("");
+    script.Print(" LCD density: %s"%(density));
+    script.Print("");
+    script.Print(" *******************************************");
+  
+  if OPTIONS.wipe_user_data:
+    system_progress -= 0.1
+  if HasVendorPartition(input_zip):
+    system_progress -= 0.1
+
+  if not OPTIONS.wipe_user_data:
+    script.AppendExtra("if is_mounted(\"/data\") then")
+    script.ValidateSignatures("data")
+    script.AppendExtra("else")
+    script.Mount("/data")
+    script.ValidateSignatures("data")
+    script.Unmount("/data")
+    script.AppendExtra("endif;")
+
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 

AospExtended/platform_build

tools/releasetools/ota_from_target_files.py
@@ -672,12 +672,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("***************************************************");
   script.Print("*                By:-ishubhamsingh                *");
   script.Print("***************************************************");
-  builddate = GetBuildProp("ro.build.date", OPTIONS.info_dict)
-  device = GetBuildProp("ro.product.model", OPTIONS.info_dict)
-  script.Print("***************************************************");
-  script.Print("        Build date: %s"%(builddate));
-  script.Print("                Device: %s "%(device));
-  script.Print("***************************************************");
   script.Print(" ")
   script.AppendExtra("sleep (2);")
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
@@ -698,13 +692,46 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
+  if GetBuildProp("ro.extended.display.version", OPTIONS.info_dict) is not None:
+    buildid = GetBuildProp("ro.extended.display.version", OPTIONS.info_dict)
+    buildidn = GetBuildProp("ro.build.id", OPTIONS.info_dict)
+    buildday = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+    securep = GetBuildProp("ro.build.version.security_patch", OPTIONS.info_dict)
+    density = GetBuildProp("ro.sf.lcd_density", OPTIONS.info_dict)
+    device = GetBuildProp("ro.aex.device", OPTIONS.info_dict)
+    androidver = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
+    manifacturer = GetBuildProp("ro.product.manufacturer", OPTIONS.info_dict)
+    sdkver = GetBuildProp("ro.build.version.sdk", OPTIONS.info_dict)
+    script.Print(" **************** Software *****************");
+    script.Print(" OS version: %s"%(buildid));
+    script.Print("");
+    script.Print(" Android version: %s"%(androidver));
+    script.Print("");
+    script.Print(" Security patch: %s"%(securep));
+    script.Print("");
+    script.Print(" SDK version: %s"%(sdkver));
+    script.Print("");
+    script.Print(" Root status: Enabled");
+    script.Print("");
+    script.Print(" Build ID: %s"%(buildidn));
+    script.Print("");
+    script.Print(" Build date: %s"%(buildday));
+    script.Print(" **************** Hardware *****************");
+    script.Print(" Device codename: %s"%(device));
+    script.Print("");
+    script.Print(" Manufacturer: %s"%(manifacturer));
+    script.Print("");
+    script.Print(" LCD density: %s"%(density));
+    script.Print("");
+    script.Print(" *******************************************");
+
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
     system_progress -= 0.1
 
-  # Place a copy of file_contexts.bin into the OTA package which will be used
-  # by the recovery program.
+# Place a copy of file_contexts.bin into the OTA package which will be used
+# by the recovery program.
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 