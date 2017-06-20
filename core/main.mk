576ac15b37459ef4890742e06cf63bfc98f6d4aa
B--B <dark.schneider.ds@gmail.com>
2017-06-20 22:32:10 +0200

    Revert "User builds: unsecure and debuggable"
    This reverts commit b6f3c67b0ed228b4056bb3c76bf7a4b6a06137aa.
    
    Conflicts:
    	core/main.mk

--- core/main.mk
+++ core/main.mk
@@ -356,12 +356,12 @@ user_variant := $(filter eng user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-  # Target is not secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+  # Target is secure in user builds.
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),user)
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
   endif
 
   ifeq ($(user_variant),userdebug)
