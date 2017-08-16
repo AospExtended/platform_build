dab8f246bfa5c54eb633406e30cd2cc15fd0796c
Luca Stefani <luca020400@lineageos.org>
2017-08-16 21:10:05 +0200

    soong_config: Add LIBART_IMG_BASE variable
    Change-Id: I1f1dde127491670c95dbdbb94bcb50178459b600
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -82,6 +82,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Override_rs_driver": "$(OVERRIDE_RS_DRIVER)",'; \
 	echo '    "Treble": $(if $(filter true,$(PRODUCT_FULL_TREBLE)),true,false),'; \
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
+	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
 	echo ''; \
 	echo '    "ArtUseReadBarrier": $(if $(filter false,$(PRODUCT_ART_USE_READ_BARRIER)),false,true),'; \
 	echo ''; \
