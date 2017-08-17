2aa91d921f18121aaa51ec7e6679a9d6134e0397
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1194,7 +1194,9 @@ endif
 
 # Include any vendor specific apicheck.mk file
 -include vendor/*/build/core/apicheck.mk
-endif
+
+# Rules for QCOM targets
+-include vendor/*/build/core/qcom_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
