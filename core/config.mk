1cead30bc1530f47b5ab5feb8239de7ac2aeda98
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add JDC build support
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357
    core: Move lineage sepolicy to device/
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -961,6 +961,17 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+ifneq ($(JDC_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/jdc/sepolicy/common/sepolicy.mk)
+
+# Include any vendor specific config.mk file
+-include $(TOPDIR)vendor/*/build/core/config.mk
+
+# Include any vendor specific apicheck.mk file
+-include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
 # Rules for QCOM targets
 -include $(TOPDIR)vendor/aosp/build/core/qcom_target.mk
 
