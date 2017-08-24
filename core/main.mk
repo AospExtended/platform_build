098a638efb3e4c2c865917123ced5403df4de45a
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-25 00:31:10 +0200

    main.mk:  build: unsecure if userdebug
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -335,17 +335,19 @@ user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-  # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),user)
+    # Target is secure in user builds.
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
   endif
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
     tags_to_install += debug
+    # Target is unsecure in user builds.
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   else
     # Disable debugging in plain user builds.
     enable_target_debugging :=
