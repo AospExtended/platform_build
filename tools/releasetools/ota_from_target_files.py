70ad9299163cf4129018bbbdbd59ee54fdb943a8
maxwen <max.weninger@gmail.com>
2018-01-02 23:22:06 +0100

    [1/2] build: add OTA script do delete package cache
    Change-Id: I6991aae2360ced58bbebd661a81b2d2c50c5467a

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -596,6 +596,10 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("BackupTools: DONE!")
     script.Unmount("/system")
 
+  script.Mount("/system")
+  script.RunCleanCache()
+  script.Unmount("/system")
+
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
