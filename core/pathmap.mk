f5db9a17fb768af7737f57701c08f4fc5486d923
Simon Shields <simon@lineageos.org>
2018-03-02 12:55:47 +1100

    build: unconditionally use pathmap
    even if we're not building a aosp target, we need to
    use the pathmap to make things like recovery and ril
    happy
    
    Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -93,6 +93,4 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
-ifneq ($(CUSTOM_BUILD),)
 -include $(TOPDIR)vendor/aosp/build/core/pathmap.mk
-endif
