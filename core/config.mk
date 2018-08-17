878592e83881081be61fcae9c7f8f03b9be80a61
Shubham Singh <coolsks94@gmail.com>
2018-08-17 01:21:21 +0000

    [TEMP] Don't include custom sepolicy
    Change-Id: I4be8b9637a423839ae34da4f5a2681711a290ac5
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1095,7 +1095,7 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/aosp/sepolicy/common/sepolicy.mk)
+#$(eval include device/aosp/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
