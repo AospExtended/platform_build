bd7f8248eb57bb8be47db3721890eea3b0554bc5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-09 22:21:19 -0300

    build: Add others targets on lunch menu [1/2]
    Change-Id: Idb5ed98634de1d6d2c8dade0811578c1d2a5f944
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -642,7 +642,7 @@ function print_lunch_menu()
 
     local i=1
     local choice
-    for choice in $(echo $choices)
+    for choice in ${choices[@]}
     do
         echo "     $i. $choice"
         i=$(($i+1))
@@ -655,12 +655,20 @@ function lunch()
 {
     local answer
 
-    if [[ $# -gt 1 ]]; then
-        echo "usage: lunch [target]" >&2
-        return 1
-    fi
+    choices=()
+    for makefile_target in $(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES)
+    do
+        choices+=($makefile_target)
+    done
+    for other_target in ${lunch_others_targets[@]}
+    do
+        if [[ " ${choices[*]} " != *"$other_target"* ]];
+        then
+            choices+=($other_target)
+        fi
+    done
 
-    if [ "$1" ]; then
+    if [ "$1" ] ; then
         answer=$1
     else
         print_lunch_menu
@@ -675,7 +683,6 @@ function lunch()
         selection=aosp_arm-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
-        local choices=($(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES))
         if [ $answer -le ${#choices[@]} ]
         then
             # array in zsh starts from 1 instead of 0.
