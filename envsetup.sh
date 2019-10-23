fb3d14bbfa45a9d5176e5d72dbceffb0cf8b36bf
Aayush Gupta <aayushgupta219@gmail.com>
2019-10-23 05:22:33 +0000

    build: Show time taken for execution of mka
    Change-Id: I851394adde83ec1bb3956eb8b4c143dba5904b79

--- envsetup.sh
+++ envsetup.sh
@@ -1453,10 +1453,10 @@ function _complete_android_module_names() {
 function mka() {
     case `uname -s` in
         Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            time make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+            time schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
             ;;
     esac
 }
