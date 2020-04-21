76b85dcfe196224699ac2af4c7a71ace8d702488
ShubhamB <shubhamprince111@gmail.com>
2020-04-21 07:08:20 +0000

    build: Ignore Ignore neverallows
    This reverts commit 5bea8e5262986f6a42eed57fe8df3c3e67c72c33.
    
    Change-Id: I75c8af21ba31d804c1383ffaca12ac99cc1369b1

--- core/config.mk
+++ core/config.mk
@@ -1193,11 +1193,6 @@ $(eval include device/aosp/sepolicy/common/sepolicy.mk)
 # Include any vendor specific apicheck.mk file
 -include vendor/*/build/core/apicheck.mk
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for QCOM targets
 -include vendor/*/build/core/qcom_target.mk
 
@@ -1206,4 +1201,3 @@ endif
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
-
