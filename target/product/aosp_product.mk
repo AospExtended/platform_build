sb6596/platform_build-1

product: Exclude AOSP apn list on AEX build
Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

target/product/aosp_product.mk
@@ -36,8 +36,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(AEX_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
 
 # NFC:
 #   Provide a libnfc-nci.conf to GSI product

target/product/full_base_telephony.mk
@@ -24,8 +24,12 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dataroaming=true
 
 PRODUCT_COPY_FILES := \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
+ifeq ($(AEX_BUILD),)
+PRODUCT_COPY_FILES += \
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)

target/product/mainline.mk
@@ -36,4 +36,6 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
+ifeq ($(AEX_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif

AospExtended/platform_build

target/product/aosp_product.mk
@@ -36,8 +36,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(AOSP_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
 
 # NFC:
 #   Provide a libnfc-nci.conf to GSI product

target/product/full_base_telephony.mk
@@ -24,8 +24,12 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dataroaming=true
 
 PRODUCT_COPY_FILES := \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
+ifeq ($(AOSP_BUILD),)
+PRODUCT_COPY_FILES += \
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)

target/product/mainline.mk
@@ -36,4 +36,6 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
+ifeq ($(AOSP_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif